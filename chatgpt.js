dc0f089faea2445b0d859340376ff014d69eb30e
hanwool <soink3667@naver.com>
2023-11-02 14:19:37 +0900

    upgrade gpt 3.5 to 4

--- chatgpt.js
+++ chatgpt.js
@@ -12,9 +12,14 @@ export async function talking(prompt) {
     const openai = new OpenAIApi(configuration);
 
     const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4",
         messages: prompt
     });
 
     return completion.data;
 }
+
+
+// GPT 3.5버전 엔진: gpt-3.5-turbo
+// 만약 gpt-4로 바꾸면 max token수 늘어나니 데이터베이스 content VARCHAR량 늘려야함!!!!
+// gpt-4는 8192토큰까지 가능 하지만 비싸서 4096까지만 global_variables.js에 해놓음
