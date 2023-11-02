c5040a38b0d2b23658c9241eca375cb5872976a8
hanwool <soink3667@naver.com>
2023-11-02 14:19:08 +0900

    replace ip to domain

--- global_variables.js
+++ global_variables.js
@@ -1,4 +1,4 @@
 export const port = 3000;
-export const ip = '221.167.5.173';
+export const ip = 'hanwool.xyz';
 export const project_dir = '/home/hanwool/kakaotalk_bot';
 export const recall_gpt_max_tokens = 3072;
